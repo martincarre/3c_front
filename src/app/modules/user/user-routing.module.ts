c3a51b4f518669a946ac0f5e7e2ac748a70e3ac6
Martin Carre <m.carre86@gmail.com>
2023-11-21 09:51:11 +0100

    Keeping up with basic setting

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedRoutingModule } from './shared-routing.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    SharedRoutingModule
+    CommonModule
   ]
 })
-export class SharedModule { }
+export class UserRoutingModule { }
