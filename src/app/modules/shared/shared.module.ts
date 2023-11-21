66a8a81f4f3e8ee539106f9cf833eb276a8786d2
Martin Carre <m.carre86@gmail.com>
2023-11-21 13:12:08 +0100

    basic navbar

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,14 +1,31 @@
+// Basic Module imports:
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 
+// Shared Components:
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+// Bootstrap imports:
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    // Bootstrap imports:
+    NgbDropdownModule
+  ],
+  exports: [
+    // Shared Components:
+    NavbarComponent,
+    // Bootstrap exports:
+    NgbDropdownModule
   ]
 })
 export class SharedModule { }
