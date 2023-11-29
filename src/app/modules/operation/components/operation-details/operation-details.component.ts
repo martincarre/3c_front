789546487c803b8fc2cf6a2b25cb8d8846eb70ac
Martin Carre <m.carre86@gmail.com>
2023-11-29 12:35:57 +0100

    Starting to work on operations

---
+++ src/app/modules/operation/components/operation-details/operation-details.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-operation-details',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './operation-details.component.html',
+  styleUrl: './operation-details.component.scss'
+})
+export class OperationDetailsComponent {
+
+}
