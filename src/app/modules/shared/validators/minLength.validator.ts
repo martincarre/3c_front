ade0d49ca41604c135d0383bc75432b4322a2ac1
Martin Carre <m.carre86@gmail.com>
2024-02-16 08:34:19 +0100

    weird commit

--- src/app/modules/shared/validators/minLength.validator.ts
+++
@@ -1,5 +0,0 @@
-import { FormlyFieldConfig } from "@ngx-formly/core";
-
-export function minLengthValidationMessages(error: any, field: FormlyFieldConfig) {
-    return `Tiene que tener al menos ${field.props!['minLength']} caracteres`;
-}
